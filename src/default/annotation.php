6f901942f6d5c40d6cae1c22f9faf6089465519d
冷斌 <xykxyk2008@163.com>
2019-08-29 14:33:48 +0800

    fix bug

--- src/default/annotation.php
+++ src/default/annotation.php
@@ -9,11 +9,29 @@ function isJson($string)
     return (json_last_error() == JSON_ERROR_NONE) ? is_array($data) : '';
 }
 
-function format($params, $type = 0, $exampleItems = [])
+function getExample($items, $definitions)
+{
+    $itemData = [];
+    foreach ($items as $item) {
+        if ($item['type'] === 'object') {
+            $item['example'] = getExample($definitions[$item['ref']], $definitions);
+        }
+        if ($item['type'] === 'array') {
+            $item['example'] = [getExample($definitions[$item['ref']], $definitions)];
+        }
+        $itemData[$item['property']] = $item['example'];
+    }
+    return $itemData;
+}
+
+function format($params, $type = 0, $definitions = [])
 {
     $data = [];
     foreach ($params as $filed => $value) {
         if (is_null($value)) {
+            continue;
+        }
+        if ($filed === 'ref' && empty($value)) {
             continue;
         }
         if (in_array($value, ['true', 'false']) || ($filed === 'example' && isJson($value))) {
@@ -26,14 +44,19 @@ function format($params, $type = 0, $exampleItems = [])
             if ($type === 1) {
                 $string = '@SWG\Schema(' . $string . ')';
             }
-            if ($type === 2) {
-                $string = 'items={"$ref":"#/definitions/'.$value.'"}';
-                $itemData = [];
-                foreach ($exampleItems as $item) {
-                    $itemData[$item['property']] = $item['example'];
+            if (in_array($type, [2, 3])) {
+                $exampleItems = $definitions[$value];
+                if ($type === 2) {
+                    $string = 'items={"$ref":"#/definitions/' . $value . '"}';
+                    $itemData = [getExample($exampleItems, $definitions)];
                 }
-                if (!empty($itemData)) {
-                    $data[2] = 'example={' . json_encode($itemData, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE) . '}';
+                if ($type === 3) {
+                    $itemData = getExample($exampleItems, $definitions);
+                }
+                $item = json_encode($itemData, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+                $item = str_replace(['[', ']'], ['{', '}'], $item);
+                if (!empty($item)) {
+                    $data[2] = 'example=' . $item;
                 }
             }
         }
@@ -80,12 +103,14 @@ function getDefinitions($definitions)
     foreach ($definitions as $name => $definition) {
         $definitionItems = [];
         foreach ($definition as $datum) {
-            $type = $datum['type'] === 'array' ? 2 : 0;
-            $item = [];
-            if ($type) {
-                $item = $definitions[$datum['ref']];
+            $type = 0;
+            if ($datum['type'] === 'array') {
+                $type = 2;
             }
-            $definitionItem = implode(",\n *        ", format($datum, $type, $item));
+            if ($datum['type'] === 'object' && $datum['property'] !== 'data') {
+                $type = 3;
+            }
+            $definitionItem = implode(",\n *        ", format($datum, $type, $definitions));
             $definitionItems[] = <<<definitionItems
  *                
  *    @SWG\Property(
